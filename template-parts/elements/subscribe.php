2057cc870ca700267dee2a5a340af208069a3957
Gary McNally <garymcnally@garys-mbp.home>
2020-05-31 21:16:03 +0100

    subscribe box added

---
+++ template-parts/elements/subscribe.php
@@ -0,0 +1,12 @@
+<div class="container-fluid subscribe">
+	<div class="container ">
+		<div class="row">
+			<div class="col-md-12">
+				<h3>Subscribe <span>for regular product and information updates</span></h3>
+				<div class="col-md-8 col-md-offset-2">
+					<?php echo do_shortcode('[contact-form-7 id="66" title="Subscribe"]'); ?>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
